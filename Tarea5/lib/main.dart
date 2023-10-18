da82840685260d1e6c6d753e5d4b6096ced81141
IvanPaul1515 <ipmx0001@ce.pucmm.edu.do>
2023-10-17 20:14:24 -0400

    subiendo tarea 5

---
+++ Tarea5/lib/main.dart
@@ -0,0 +1,92 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int currentIndex = 0;
+  int currentImageIndex = 0;
+  List<String> backgroundImages = [
+    "assets/fondo1.png",
+    "assets/fondo2.png",
+    // Fondos
+  ];
+  List<String> displayTexts = [
+    "? ? ?",
+    "IngenieroEnSistemaSaurio",
+    // Textos
+  ];
+  List<String> centerImages = [
+    "assets/descarga.png",
+    "assets/ING.png",
+    // Imágenes
+  ];
+
+  void changeTextAndBackground() {
+    setState(() {
+      currentIndex = (currentIndex + 1) % backgroundImages.length;
+      currentImageIndex = (currentImageIndex + 1) % centerImages.length;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Primera APP Flutter"),
+      ),
+      body: Stack(
+        children: <Widget>[
+          Image.asset(
+            backgroundImages[currentIndex], // Imagen de fondo actual
+            width: double.infinity,
+            height: double.infinity,
+            fit: BoxFit.cover,
+          ),
+          Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Container(
+                  width: 200,
+                  height: 200,
+                  color: Colors.blue.withOpacity(0.0), // Fondo del contenedor
+                  child: Image.asset(
+                    centerImages[currentImageIndex], // Ruta de la imagen actual
+                    width: 100,
+                    height: 100,
+                  ),
+                ),
+                SizedBox(height: 20),
+                Text(
+                  displayTexts[currentIndex], // Texto actual
+                  style: TextStyle(fontSize: 24, color: Colors.white),
+                ),
+                SizedBox(height: 20),
+                ElevatedButton(
+                  onPressed: changeTextAndBackground,
+                  child: Text("Presione aquí"),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
